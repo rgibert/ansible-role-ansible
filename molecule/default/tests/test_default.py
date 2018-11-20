ae863d5fb8e23ec1241d99f5746ac4a2ade8e305
Richard Gibert <richard@gibert.ca>
2018-11-19 20:49:24 -0500

    fix tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,7 +6,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_os_installed(host):
-    assert host.file('/usr/bin/ansible').exists
-    assert host.file('/usr/bin/ansible-lint').exists
-    assert host.file('/usr/bin/molecule').exists
+def test_bin_ansible(host):
+    assert host.run('ansible --version').rc == 0
+
+
+def test_bin_ansible_lint(host):
+    assert host.run('ansible-lint --version').rc == 0
+
+
+def test_bin_molecule(host):
+    assert host.run('molecule --version').rc == 0
